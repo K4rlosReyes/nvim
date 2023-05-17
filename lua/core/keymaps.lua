bf2dc47d733538eb9f5b0e62d32b2a221b926552
K4rlosReyes <carlosreyesml18@gmail.com>
2023-05-17 10:09:51 -0400

    dev: core modifications

--- lua/core/keymaps.lua
+++ lua/core/keymaps.lua
@@ -7,6 +7,9 @@ keymap.set("n", "<C-j>", "<C-w>j")
 keymap.set("n", "<C-k>", "<C-w>k")
 
 keymap.set("n", "<leader>ee", ":Rexplore<CR>")
+keymap.set("n", "<A-v>", ":ToggleTerm size=40 direction=vertical<CR>")
+keymap.set("n", "<A-f>", ":ToggleTerm size=10 direction=float<CR>")
+keymap.set("n", "<A-h>", ":ToggleTerm size=20 direction=horizontal<CR>")
 
 function _G.set_terminal_keymaps()
     local opts = {buffer = 0}
@@ -17,12 +20,6 @@ function _G.set_terminal_keymaps()
     vim.keymap.set('t', '<C-k>', [[<Cmd>wincmd k<CR>]], opts)
     vim.keymap.set('t', '<C-l>', [[<Cmd>wincmd l<CR>]], opts)
     vim.keymap.set('t', '<C-w>', [[<C-\><C-n><C-w>]], opts)
-    vim.keymap.set("n", "<A-v>", ":ToggleTerm size=40 direction=vertical<CR>",
-                   opts)
-    vim.keymap
-        .set("n", "<C-t>", ":ToggleTerm size=10 direction=float<CR>", opts)
-    vim.keymap.set("n", "<A-h>", ":ToggleTerm size=20 direction=horizontal<CR>",
-                   opts)
 end
 
 -- if you only want these mappings for toggle term use term://*toggleterm#* instead
--- lua/core/options.lua
+++ lua/core/options.lua
@@ -10,7 +10,7 @@ opt.cursorline = false
 
 -- Indenting
 opt.expandtab = true
-opt.shiftwidth = 2
+opt.shiftwidth = 4
 opt.smartindent = true
 opt.tabstop = 4
 opt.softtabstop = 4
@@ -33,13 +33,11 @@ opt.termguicolors = true
 opt.timeoutlen = 400
 opt.undofile = true
 
--- interval for writing swap file to disk, also used by gitsigns
 opt.updatetime = 250
 
--- go to previous/next line with h,l,left arrow and right arrow
--- when cursor reaches end/beginning of line
 opt.whichwrap:append "<>[]hl"
 
 vim.g.python3_host_prog = "~/Apps/miniconda3/envs/torch/bin/python"
+vim.g.python3 = "~/Apps/miniconda3/envs/torch/bin/python"
 
 vim.g.mapleader = " "
